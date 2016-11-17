64d7fa602809f8a3431fa2b4bd0ddb5d885f956f
sarareibschied <SR251148@muhlenberg.edu>
2016-11-16 19:21:09 -0500

    saving on Nov 16

--- csi185website/functional_tests.py
+++ csi185website/functional_tests.py
@@ -2,6 +2,8 @@
 
 # Boiler Plate
 
+# Can type localhost:8000/index.html into browser to see it
+
 
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
@@ -45,33 +47,19 @@ class NewVisitorTest(unittest.TestCase):
         # You will have an image for your home page I am assuming.
         # Put the name of your image here in place of homebrew.png
         # In general this is how we check for images on a page.
+        # The user sees an image of hands holding flowers.
 
         m=self.browser.find_element_by_tag_name('img')
-        self.assertIn('homebrew.png',m.get_attribute('src'))
+        self.assertIn('hands.jpg',m.get_attribute('src'))
 
-        # We check here for the title of your home page.
-        # uncomment the next lines and change the text when you set your title.
-        # put your title in place of "The Title of My Home Page"
-
-        h=self.browser.find_element_by_css_selector('h1')
-        self.assertIn("Bridging the Gap to Reality",h.text)
-
-        # There is an area specified around the computer keyboard.
-        # the 'id' of this area is 'keyboard'
-
-        # this is how we find that area.
-        a=self.browser.find_element_by_id('keyboard')
-
-        # this is how we click on it.
+        a=self.browser.find_element_by_id('flowers')
         a.click()
 
-        # after clicking on it, we should see the next page.
-        e=self.browser.find_element_by_css_selector("h1")
-        self.assertIn('Keyboards',e.text)
+        self.assertIn('Flowers',self.browser.title)
 
-        # The page should have a picture of a teletype machine. 
+        h=self.browser.find_element_by_tag_name('h1')
         m=self.browser.find_element_by_tag_name('img')
-        self.assertIn('teletype.jpg',m.get_attribute('src'))
+
     
 
 if __name__=="__main__":
